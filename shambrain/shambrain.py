8aceec14a902490cff7cd190f782ec8e2c7cc90e
oliver-contier <oliver.contier@ovgu.de>
2017-05-23 15:07:37 -0400

    Wrote function get_filepaths_bids and restructured run4bids function. Also added some docstrings.

--- shambrain/shambrain.py
+++ shambrain/shambrain.py
@@ -8,6 +8,22 @@ Simulate fMRI run with no activation.
 
 
 def simulate_run(infile, outfile, lfnl=3.0, hfnl=None):
+    """
+    Simple simulation of 4D fmri data. Takes a given functional image,
+    performs motion correction, computes the mean and adds autocorrelated
+    noise to it.
+
+    Parameters
+    ----------
+    infile:     str
+        Path to the 4D functional image in .nifti format
+    outfile:    str
+        Path and name of the simulated image
+    lfnl:       float
+        Low frequency noise level. Default = 3 Hz
+    hfnl:       float
+        High frequency noise level. Default = None.
+    """
 
     from nipype.interfaces import fsl
     fsl.FSLCommand.set_default_output_type('NIFTI_GZ')
@@ -43,19 +59,58 @@ def simulate_run(infile, outfile, lfnl=3.0, hfnl=None):
     image.to_filename(outfile)
 
 
-def run4bids(bids_dir):
+def get_filepaths_bids(bids_dir):
+    """
+    Get the input and outputh pathnames given a BIDS dataset. 
 
+    Parameters
+    ----------
+    bids_dir:   str
+        Base directory of the BIDS data set. Should contain the subject
+        folders ('sub-001' etc.)
+
+    Returns
+    -------
+    infiles:    list
+        List of path names for all files in the subjects' 'func' subdirectory
+    outfiles:   list
+        List of path names for the to be saved simulated functinoal images.
+        Will be saved under '/derivatives/sub-XX/sham/'.
+    """
     import os
 
-    subdirs = [directory for directory in os.listdir(bids_dir) if directory.startswith('sub-')]
+    infiles, outfiles = [], []
+
+    subdirs = [directory for directory in os.listdir(bids_dir)
+               if directory.startswith('sub-')]
 
     for sub in subdirs:
 
-        funcs = [dircontent for dircontent in os.listdir(os.path.join(bids_dir, sub, 'func'))
+        funcs = [dircontent for dircontent in
+                 os.listdir(os.path.join(bids_dir, sub, 'func'))
                  if dircontent.endswith('.nii.gz')]
 
         for func in funcs:
-            in_file = os.path.join(bids_dir, sub, 'func', func)
-            out_file = os.path.join(bids_dir, 'derivatives', sub, 'shambrain',
-                                    func.replace('.nii.gz', '_sham.nii.gz'))
-            simulate_run(in_file, out_file)
+            infiles.append(os.path.join(bids_dir, sub, 'func', func))
+            outfiles.append(os.path.join(
+                bids_dir, 'derivatives', sub, 'shambrain',
+                func.replace('.nii.gz', '_sham.nii.gz')))
+
+    return infiles, outfiles
+
+
+def run4bids(bids_dir):
+    """
+    runs simulation for all functional images in a BIDS data set.
+    
+    Parameters
+    ----------
+    bids_dir:   str
+        Base directory of the BIDS data set. Should contain the subject
+        folders ('sub-001' etc.)
+    """
+
+    for inf, outf in zip(get_filepaths_bids(bids_dir)):
+        simulate_run(inf, outf)
+
+# TODO: function to create a condor submission file
